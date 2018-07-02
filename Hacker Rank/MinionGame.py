cb480c094a2460970d7ae2d8160934720cac1c60
kevalrathod <kevalrathod1111@gmail.com>
2018-07-02 14:12:06 +0530

    minion game

--- Hacker Rank/MinionGame.py
+++ Hacker Rank/MinionGame.py
@@ -1,31 +1,23 @@
-# This  code is not correct.!
-
-#under process
 
 def minion_game(string):
-	#your code oes here
-	vowel=['a','e','i','o','u','A','E','I','O','U']
-	count=0;
-	substring=string[0:1]
-	count_kevin=0
-	count_stuart=0
-		for z in string:
-			if z in vowel:
-				for j in range(0,len(string):
-					if string[j:j+len(substring)]==substring:
-						count_kevin=count_kevin+1
-			else:
-				for y in range(0, len(string)):
-					if string[y:y+len(substring)]==substring:
-						count_stuart=count_stuart+1
+    # your code goes here
+    kevin=0
+    stuart=0
+    n=len(string)
+    for i in range(0,len(string)):
+        if string[i] in {'A','E','I','O','U'}:
+            kevin=kevin+n-i
+        else:
+            stuart=stuart+n-i
 
+    if kevin>stuart:
+        print('Kevin',kevin)
+    elif kevin==stuart:
+        print('Draw')
+    else:
+        print('Stuart', stuart)
 
-
-	if count_kevin > count_stuart:
-		print("Kevin", count_kevin)
-	else:
-		print("Stuart" , count_stuart)
 
 if __name__ == '__main__':
-	s=input()
-	minion_game(s)
+    s = raw_input()
+    minion_game(s)
